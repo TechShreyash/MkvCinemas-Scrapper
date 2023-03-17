746b33c7c5b560157c646d1d96ea80d4fdaecdad
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2023-03-17 12:09:31 +0530

    MkvCinemas Scrapper

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# MkvCinemas Scrapper
- MkvCinemas Scrapper Powered By TechZApi
+## MkvCinemas Scrapper
+
+MkvCinemas Scrapper Powered By TechZApi
+
+### Requirements
+
+API_KEY : Create your own key, from [here](https://telegram.me/TechZApiBot)
+
+### Usage
+
+- Fill Api Key in main.py
+- Run main.py
+- Enter url of movie or series
+- This script will return you the gdtot links of files
---
+++ main.py
@@ -0,0 +1,39 @@
+# You must have installed latest version techzapi wrapper on your pc
+# pip install -U techzapi
+
+from techzapi.api import TechZApi
+import time
+
+API_KEY = "Your API Key"
+scrapper = TechZApi.MkvCinemas(API_KEY)
+
+url = input("Enter url of any movie or series: ")
+max = 3  # Max no. of links to scrap
+
+print("\nAdding task to queue...")
+data = scrapper.add_task(url, max)
+queue = data["queue"]
+hash = data["hash"]
+print(f"\nAdded task to queue, Queue Position : {queue}")
+
+while True:
+    time.sleep(15)
+    print("\n", "Checking queue...".center(50, "="))
+    data = scrapper.get_task(hash)
+
+    if data["status"] == "pending":
+        print(f'Status: Pending || Queue Position : {data["queue"]}')
+        continue
+
+    if data["status"] == "processing":
+        print(f'Status : Processing || Links Scrapped : {data["scrapped"]}')
+        continue
+
+    if data["status"] == "completed":
+        print("Task completed...")
+        print(f'Links: {data["results"]}')
+        break
+
+    if data["status"] == "failed":
+        print("Task failed...")
+        break
